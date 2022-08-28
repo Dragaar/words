f273b1fc722a6ba04f5c35180db53b8b616f74ac
Dragaar <rosivanyshyn@gmail.com>
2022-08-28 14:54:38 +0300

    222
    simple path transforming

--- src/main/java/com/epam/rd/autotasks/words/StringUtil.java
+++ src/main/java/com/epam/rd/autotasks/words/StringUtil.java
@@ -1,70 +1,85 @@
 package com.epam.rd.autotasks.words;
 
 import java.util.Arrays;
+import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringUtil {
     public static int countEqualIgnoreCaseAndSpaces(String[] words, String sample) {
-
 
         if(words == null || sample == null || sample == "") return  0;
         int countEqualsWord = 0;
 
         //для кожного слова з words
         for(int i = 0; i < words.length; i ++) {
-
-
-
-            if(words[i].replaceAll("\\s*", "")
+            //заміна символів пустої строки лишень на початку та кінці
+            if(words[i].trim() //replaceAll("\\s*", "")
                     .compareToIgnoreCase(
-                            sample.replaceAll("\\s*", "")
+                            sample.trim()
                     )
                == 0) countEqualsWord++;
-           // System.out.println(words[i].replaceAll("\\s*", "").toString());
+           System.out.println(words[i].trim().toString());
         }
-        System.out.println(sample);
+        //System.out.println(sample);
         return countEqualsWord;
     }
 
     public static String[] splitWords(String text) {
-        throw new UnsupportedOperationException();
+        if(text == null || text == "") return null;
+
+        Pattern p1 = Pattern.compile("[ ,.;:?!]");
+        String[] temp = p1.split(text);
+
+        StringJoiner result = new StringJoiner(",");
+        for(String e : temp) if(e != "") result.add(e);
+        if(result.length() == 0) return null;
+
+        printString(p1.split(result.toString()));
+        return p1.split(result.toString());
     }
 
     public static String convertPath(String path, boolean toWin) {
-        throw new UnsupportedOperationException();
+
+        String resultPath = null;
+
+        String checkWinPath = "";
+        String checkUnixPath = "~?[\\B\\]";
+
+        //Pattern regexToWin = Pattern.compile("(~?)([//])");
+        //Pattern regexToUnix = Pattern.compile("");
+        System.out.println(path);
+        if(toWin)
+       {
+           //Matcher m = regexToWin.matcher(path);
+           if(path.startsWith("~"))         resultPath = path.replaceFirst("\\~", "C:\\\\User").replaceAll("/", "\\\\");
+           else if (path.startsWith("/"))   resultPath = path.replaceFirst("/", "C:\\\\").replaceAll("/", "\\\\");
+           else resultPath = path.replaceAll("/", "\\\\");
+
+       }
+       else
+       {
+           if(path.startsWith("C")) resultPath = path.replaceFirst("C\\:\\\\\\\\User", "~/").replaceAll("\\\\", "/");
+           else resultPath = path.replaceAll("\\\\", "/");
+       }
+        System.out.println(resultPath);
+       return resultPath;
     }
 
     public static String joinWords(String[] words) {
-        throw new UnsupportedOperationException();
+        if(words == null) return null;
+
+        StringJoiner result = new StringJoiner(", ");
+        for(String e : words) if(e != "") result.add(e);
+        if(result.length() == 0) return null;
+
+        return "["+result.toString()+"]";
     }
 
-    public static void main(String[] args) {
-        System.out.println("Test 1: countEqualIgnoreCaseAndSpaces");
-        String[] words = new String[]{" WordS    \t", "words", "w0rds", "WOR  DS", };
-        String sample = "words   ";
-        int countResult = countEqualIgnoreCaseAndSpaces(words, sample);
-        System.out.println("Result: " + countResult);
-        int expectedCount = 2;
-        System.out.println("Must be: " + expectedCount);
-
-        System.out.println("Test 2: splitWords");
-        String text = "   ,, first, second!!!! third";
-        String[] splitResult = splitWords(text);
-        System.out.println("Result : " + Arrays.toString(splitResult));
-        String[] expectedSplit = new String[]{"first", "second", "third"};
-        System.out.println("Must be: " + Arrays.toString(expectedSplit));
-
-        System.out.println("Test 3: convertPath");
-        String unixPath = "/some/unix/path";
-        String convertResult = convertPath(unixPath, true);
-        System.out.println("Result: " + convertResult);
-        String expectedWinPath = "C:\\some\\unix\\path";
-        System.out.println("Must be: " + expectedWinPath);
-
-        System.out.println("Test 4: joinWords");
-        String[] toJoin = new String[]{"go", "with", "the", "", "FLOW"};
-        String joinResult = joinWords(toJoin);
-        System.out.println("Result: " + joinResult);
-        String expectedJoin = "[go, with, the, FLOW]";
-        System.out.println("Must be: " + expectedJoin);
+    private static void printString(String[] s)
+    {
+        System.out.println();
+        for(String e : s) System.out.print("("+e.toString() + ") ");
+        System.out.println();
     }
 }
